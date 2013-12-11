c3ca54cb4cf928ed07bf715b2064a8ec3be15065
Vlad Nikiporoff <vlad@initech.co.il>
2013-12-11 23:32:04 +0200

    local changes

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,9 +8,9 @@
 
 dir=~/dotfiles                    # dotfiles directory
 prev_dir=~/_dotfiles            # old dotfiles backup directory
+
 files="vim screens bash_history bash_logout bashrc inputrc profile screenbashrc screenbashrcgit screendefbashrc screenrc shellsrc vimrc"    # list of files/folders to symlink in homedir
 just_files="mTouch.py"
-#dirs="vim screens"
 ##########
 
 # create dotfiles_old in homedir
@@ -40,30 +40,3 @@ for file in $just_files; do
     ln -s $dir/$file ~/$file
 done
 
-# install_zsh () {
-# # Test to see if zshell is installed.  If it is:
-# if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-#     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-#     if [[ ! -d $dir/oh-my-zsh/ ]]; then
-#         git clone http://github.com/michaeljsmalley/oh-my-zsh.git
-#     fi
-#     # Set the default shell to zsh if it isn't currently set to zsh
-#     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-#         chsh -s $(which zsh)
-#     fi
-# else
-#     # If zsh isn't installed, get the platform of the current machine
-#     platform=$(uname);
-#     # If the platform is Linux, try an apt-get to install zsh and then recurse
-#     if [[ $platform == 'Linux' ]]; then
-#         sudo apt-get install zsh
-#         install_zsh
-#     # If the platform is OS X, tell the user to install zsh :)
-#     elif [[ $platform == 'Darwin' ]]; then
-#         echo "Please install zsh, then re-run this script!"
-#         exit
-#     fi
-# fi
-# }
-# 
-# install_zsh
